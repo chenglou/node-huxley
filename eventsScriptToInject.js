e61344c8f5ba5ff59c338445da97c246f2ea38ba
Cheng Lou <chenglou92@gmail.com>
2013-08-21 10:22:54 -0400

    script to inject onto the page to track user events

---
+++ eventsScriptToInject.js
@@ -0,0 +1,18 @@
+// the script that will be injected into the page for listening to user input
+(function() {
+  var events = [];
+
+  window.addEventListener('click', function(e) {
+    events.push([Date.now(), 'click', [e.clientX, e.clientY]]);
+  }, true);
+
+  window.addEventListener('keyup', function(e) {
+    events.push([Date.now(), 'keyup', String.fromCharCode(e.keyCode)]);
+  }, true);
+
+  // TODO: double click (?), right click
+
+  window._getHuxleyEvents = function() {
+    return events;
+  };
+})();
