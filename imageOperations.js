8fd1a33bce450b546259ecda2936e4b4fcf10819
Cheng Lou <chenglou92@gmail.com>
2013-08-28 13:04:00 -0400

    remove gm dependency
    Only used for comparing image diffs. Inlined the code.

--- imageOperations.js
+++ imageOperations.js
@@ -1,4 +1,3 @@
-var gm = require('gm');
 var fs = require('fs');
 var exec = require('child_process').exec;
 
@@ -13,20 +12,14 @@ function compareAndSaveDiffOnMismatch(image1Buffer, image2Path, taskPath, done) 
   var tempFileName = 'temp' + Math.random() + '.png';
   writeToFile(tempFileName, image1Buffer, function(err) {
     try {
-      // pixel perfect, leaves no room for error (the 0)
-      gm.compare(tempFileName, image2Path, 0, function(err, areSame) {
-        // TODO: don't throw
+      _checkIfDifferent(tempFileName, image2Path, function(err, areSame) {
         if (!areSame) {
-          // waiting for gm to accept the pull on diff saving. Meanwhile...
-          var diffPngPath = taskPath + '/diff.png';
-          exec(
-            'gm compare -file ' + diffPngPath + ' ' + tempFileName + ' ' + image2Path,
-            function(err) {
-              fs.unlink(tempFileName, function() {
-                done(err, areSame);
-              });
-            }
-          );
+          var diffPath = taskPath + '/diff.png';
+          _saveDiffImage(tempFileName, image2Path, diffPath, function(err) {
+            fs.unlink(tempFileName, function(err2) {
+              done(err || err2, areSame);
+            });
+          });
         } else {
           fs.unlink(tempFileName, function() {
             done(null, areSame);
@@ -38,6 +31,32 @@ function compareAndSaveDiffOnMismatch(image1Buffer, image2Path, taskPath, done) 
       throw e;
     }
   });
+}
+
+function _checkIfDifferent(image1Path, image2Path, done) {
+  exec('gm compare -metric mse ' + image1Path + ' ' + image2Path, function (err, stdout) {
+    // TODO: throw?
+    if (err) return done(err);
+
+    // the output is an ascii table, with the last row like this:
+    // Total: 0.0000607584        0.0
+    //           ^ what we want
+    var match = /Total: (\d+\.?\d*)/m.exec(stdout);
+    if (!match) {
+      // TODO: throw?
+      err = new Error('Unable to parse output.\nGot ' + stdout);
+      return done(err);
+    }
+
+    var equality = parseFloat(match[1]);
+    done(err, equality === 0);
+  });
+}
+
+function _saveDiffImage(image1Path, image2Path, diffPath, done) {
+  exec(
+    'gm compare -file ' + diffPath + ' ' + image1Path + ' ' + image2Path, done
+  );
 }
 
 module.exports = {
