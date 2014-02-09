ff63865b4b0171ed3ee150158abbcfb54b628b6e
Cheng Lou <chenglou92@gmail.com>
2014-02-09 12:36:31 -0800

    Fix delete/backspace key not simulated correctly
    Fixes #29.

--- source/bigBrother.js
+++ source/bigBrother.js
@@ -9,7 +9,9 @@
     '37': 'ARROW_LEFT',
     '38': 'ARROW_UP',
     '39': 'ARROW_RIGHT',
-    '40': 'ARROW_DOWN'
+    '40': 'ARROW_DOWN',
+    '8': 'BACK_SPACE',
+    '46': 'DELETE'
   };
 
   // we treat a click as a mousedown, because they behave the same way
@@ -30,10 +32,37 @@
   // only `keypress` returns the correct character. `keydown` returns `A` when
   // pressing `a`, etc.
   window.addEventListener('keypress', function(e) {
-    // chrome doesn't trigger `keypress` for arrow keys while ff does. Just
-    // handle them apart below in `keydown`
+    // chrome doesn't trigger `keypress` for arrow keys, backspace and delete.
+    // ff does. Just handle them apart below in `keydown`
     var code = e.keyCode || e.which;
+    // arrow keys
     if (code >= 37 && code <= 40) return;
+
+    // firefox detects backspace and delete on keypress while webkit doesn't
+    // we'll delegate to keydown to handle these two keys. We need to treat
+    // then as special recorded keys anyways for the selenium replay
+
+    // backspace
+    if (code === 8) return;
+
+    // delete key:
+    //   keypress:
+    //     ff: keyCode 46 and which 0
+    //     chrome: not triggered
+    //   keydown:
+    //     ff: keyCode 46 and which 46
+    //     chrome: keyCode 46 and which 46
+
+    // period key:
+    //   keypress:
+    //     ff: keyCode 0 and which 46
+    //     chrome: keyCode 46 and which 46
+    //   keydown:
+    //     ff: keyCode 190 and which 190
+    //     chrome: keyCode 190 and which 190
+
+    if (e.keyCode === 46 && e.which === 0) return;
+
     // the way dom deals with keydown/press/up and which charCode/keyCode/which
     // we receive is really screwed up. If you google "keyCode charCode which"
     // you'll see why the `or` is here
@@ -50,7 +79,7 @@
   // keypress correctly in playback mode.
   window.addEventListener('keydown', function(e) {
     var code = e.keyCode || e.which;
-    if (code < 37 || code > 40) return;
+    if ((code < 37 || code > 40) && (code !== 8 && code !== 46)) return;
     // treat it as a `keypress` for simplicity during playback simulation
     events.push({
       action: 'keypress',
